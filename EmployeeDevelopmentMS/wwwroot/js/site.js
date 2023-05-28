f20e49784f522f6c248999b41f477ae52a57444e
silviya-v00 <sisi.vasileva2709@gmail.com>
2023-05-28 22:07:28 +0300

    common: Организиране на менюто като hamburger menu

--- EmployeeDevelopmentMS/wwwroot/js/site.js
+++ EmployeeDevelopmentMS/wwwroot/js/site.js
@@ -1,5 +1,25 @@
 ﻿
 $(document).ready(function () {
+    const menu = document.querySelector(".menu");
+    const menuItems = document.querySelectorAll(".menuItem");
+    const hamburger = document.querySelector(".hamburger");
+
+    function toggleMenu() {
+        if (menu.classList.contains("showMenu")) {
+            menu.classList.remove("showMenu");
+        } else {
+            menu.classList.add("showMenu");
+        }
+    }
+
+    hamburger.addEventListener("click", toggleMenu);
+
+    menuItems.forEach(
+        function (menuItem) {
+            menuItem.addEventListener("click", toggleMenu);
+        }
+    )
+
     $('#btnUPShowPersonalInfoSection').click(function () {
         PrefillPersonalInfo();
     });
